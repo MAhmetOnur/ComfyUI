5825ec4ca7debe45cd2857e7978715cfe972abe4
MAhmetOnur <m.ahmetonur01@gmail.com>
2024-10-16 15:16:57 +0300

    concept running sh

---
+++ .github/run_concepts_halloween.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Female Concepts
+python magic_retake.py --gender female --concept "mermaid, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender female --concept "rosie the riveter, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender female --concept "daenerys targaryen, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender female --concept "wednesday addams, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender female --concept "maleficent, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender female --concept "princess leia, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender female --concept "lara croft, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender female --concept "batgirl, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender female --concept "cleopatra, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender female --concept "tinker bell, halloween costume party" --num_prompts 5 --output_path ./output
+
+# Male Concepts
+python magic_retake.py --gender male --concept "thor, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "wolverine, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "indiana jones, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "captain america, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "breaking bad's walter white, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "sherlock holmes, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "spock, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "han solo, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "king arthur, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "freddy krueger, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "rambo, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "zorro, halloween costume party" --num_prompts 5 --output_path ./output
+python magic_retake.py --gender male --concept "dracula, halloween costume party" --num_prompts 5 --output_path ./output
